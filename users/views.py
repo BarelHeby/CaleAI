8f02921784922680d1ad2e1bc4233d785abf96f5
BarelHeby <barelheby03@gmail.com>
2024-02-28 19:24:09 +0200

    fix bug

--- users/views.py
+++ users/views.py
@@ -5,19 +5,19 @@ from rest_framework.views import APIView
 
 
 class UserView(APIView):
-    def list(self, request):
+    def get(self, request):
         queryset = User.objects.all()
         serializer = UserSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    def create(self, request):
+    def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
 
-    def update(self, request, pk=None):
+    def put(self, request, pk=None):
         user = User.objects.get(pk=pk)
         serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
